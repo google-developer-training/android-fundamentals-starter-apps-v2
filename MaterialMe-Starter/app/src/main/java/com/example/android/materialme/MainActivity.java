29df577af269eab3eef93351443c28fb2ea4e570
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-04-17 06:28:31 +0530

    Implemented swipe to delete and drag drop to order.

--- MaterialMe-Starter/app/src/main/java/com/example/android/materialme/MainActivity.java
+++ MaterialMe-Starter/app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -21,8 +21,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /***
  * Main Activity for the Material Me app, a mock sports news application
@@ -55,8 +57,31 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the data.
         initializeData();
+        // Item touch helper to delete items
+        helper.attachToRecyclerView(mRecyclerView);
     }
 
+    /**
+     * Item touch helper
+     */
+    ItemTouchHelper helper=new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP|
+            ItemTouchHelper.DOWN|ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                              RecyclerView.ViewHolder target) {
+            int from=viewHolder.getAdapterPosition();
+            int to=target.getAdapterPosition();
+            Collections.swap(mSportsData,from,to);
+            mAdapter.notifyItemMoved(from,to);
+            return true;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            mSportsData.remove(viewHolder.getAdapterPosition());
+            mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+        }
+    });
     /**
      * Initialize the sports data from resources.
      */
