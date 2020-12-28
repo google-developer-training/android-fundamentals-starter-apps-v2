4cb15f1efa141f423c9fa8da97e320cf04d4fae3
Paul Smith <prsmith@ranken.edu>
2020-12-28 13:57:09 -0600

    Migrating projects to Androidx.

--- MaterialMe-Starter/app/src/main/java/com/example/android/materialme/MainActivity.java
+++ MaterialMe-Starter/app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -17,9 +17,9 @@
 package com.example.android.materialme;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
